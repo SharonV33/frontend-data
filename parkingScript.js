471c71974d654a292d675c58618b312e6391b864
sharonV33 <sharonveldman33@gmail.com>
2020-11-01 17:11:51 +0100

    initial commit

---
+++ parkingScript.js
@@ -0,0 +1,78 @@
+//defining variables
+const url1  = 'https://opendata.rdw.nl/resource/b3us-f26s.json'
+const url2 = 'https://opendata.rdw.nl/resource/t5pc-eb34.json'
+const column1 = 'disabledaccess'
+const column2 = 'capacity'
+const column3 = 'areaid'
+const column4 = 'location'
+const column5 = 'areadesc'
+
+//fetch data from first url
+fetch(url1)
+//turn fetched data into .json
+    .then(result => {
+        return result.json()
+    })
+    .then(parkingData => {
+        //create new array with just the desired data
+        const columnArray = sortData(parkingData, column1, column2, column3)
+        //remove 0 and undefined values
+        const cleanArray = cleanData(columnArray)
+        //transform string numbers to integers
+        const arrayToInt = stringToNumber(cleanArray)
+        console.log(arrayToInt)
+    })
+//fetch data from second  resource
+fetch(url2)
+//turn fetched data into .json
+    .then(result => {
+        return result.json()
+    })
+    .then(parkingData => {
+        //create new array with just the desired data
+        const columnArray = sortData(parkingData, column3, column4, column5)
+        //remove 0 and undefined values
+        const cleanArray = cleanData(columnArray)
+        console.log('clean array 2 ', cleanArray)
+    })
+
+
+//create new array that only contains data from the desired column
+function sortData(dataArray, column1Name, column2Name, column3Name) {
+    return dataArray.map(item => Array(item[column1Name], item[column2Name], item[column3Name]))
+}
+
+//convert string data to integers
+//with help of Gijs Laarman
+function stringToNumber(stringData) {
+    // //map the array and replace the entries on position i with integers, i is declared in
+    // //the while loop where the function gets called
+    // const parsedData = stringData.map(array => {
+    //     array[i] = parseInt(array[i])
+    //     return array
+    // })
+    // return parsedData
+    const allNumbers = stringData.map(entry => {
+        if (typeof entry === String) {
+            return parseInt(entry)
+        }
+    })
+    return allNumbers
+}
+
+
+//filter all unusable values from the array
+function cleanData(array) {
+    //check if there are undefined values in the array and remove them
+    let noUndefinedArray = array.filter(function (filteredData) {
+        return filteredData[0] !== undefined
+    })
+    //check if there are 0 values in the array and remove them
+    let noZeroArray = noUndefinedArray.filter(function (filteredData) {
+        return filteredData[0] !== "0"
+    })
+    //return the cleaned array
+    return noZeroArray
+}
+
+
---
+++ readme.md
@@ -0,0 +1,51 @@
+# functional programming
+
+In this repository, I will be working on an assignment part of the tech track at CMD. This assignment will 
+be going into cleaning up data sets, working with d3 and eventually creating data visualisations.
+
+### about the project
+
+This project will consists of cleaning "dirty" datasets and making them usable for research and in the end a data visualisation. The dataset I will be using consists of student answers from a survey.
+
+Research question: What cities are most disability friendly? 
+sub question: What percentage of parking spaces is for disabilities?
+
+Data needed:
+* what garage has disability parking spaces 
+* how many parking spaces do these garages have in total
+* where are these garages located
+
+more information about my research questions can be found in [my wiki](https://github.com/SharonV33/functional-programming/wiki/Interesting-insights-in-the-RDW-dataset)
+
+### more information
+
+For more information about the work behind the code, check out my [github wiki](https://github.com/SharonV33/functional-programming/wiki) where I document my research
+
+
+### running
+
+1. clone the repo
+  ```
+  https://github.com/SharonV33/functional-programming.git
+  ```
+
+2. open the directory
+
+3. run npm install in the console in the directory
+```
+npm install
+```
+
+
+## people who helped me
+* Laurens Aarnouds
+* Gijs Laarman
+
+the guys from my support group
+* Randy Dalgliesh
+* Nathan Neelis
+* Simon Planje
+* Robin Langhorst
+* Marco Fijan
+
+MIT License
