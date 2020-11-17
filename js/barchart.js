1daefc2caaa4c8db37fb4af3190fdb06f97b9bbe
sharonV33 <sharonveldman33@gmail.com>
2020-11-17 17:13:12 +0100

    update pattern implemented

--- js/barchart.js
+++ js/barchart.js
@@ -1,14 +1,13 @@
 //code based on examples from curran
 //https://www.youtube.com/watch?v=NlBt-7PuaLk&ab_channel=CurranKelleher
 //and https://www.d3-graph-gallery.com/graph/barplot_basic.html
+//lots of thanks to chelsea doelman for helping me with my update pattern
 
 import { max } from 'd3'
-
 export default function barChart(dataFromBar, selected) {
     //select element to create bar chart in
     let data = dataFromBar
     const container = d3.select("#vis")
-
     const svg = container.append("svg").attr("class", "bar")
     //set width and height equal to that of the svg element
     const margin = {top: 10, right: 0, bottom: 70, left: 30}
@@ -17,82 +16,30 @@ export default function barChart(dataFromBar, selected) {
     //create x axis
     let xAxis = d3.scaleBand()
         .range([0, width])
+        .domain(data.map(data => data.name))
         //give each bar some padding
         .padding(0.5)
     //create Y axis
     let yAxix = d3.scaleLinear()
         .domain([0, max(data, data => data.isDisabled)])
         .range([height, 0])
-
     //clear currently loaded chart
     d3.select("#vis")
         .select(".pie")
         .selectAll("*")
         .remove()
-
     d3.select("#legend")
         .style("border", "none")
         .selectAll("*")
         .remove()
-
     //change colour of current button
     d3.select("#workingButtons")
         .selectAll("button")
         .style("background-color", "#FFF")
-
     d3.select("#" + selected)
         .style("background-color", "#98abc5")
-
     d3.select('#filter')
         .on("click", filterEmpty)
-
-    filterEmpty()
-
-
-    function filterEmpty() {
-        const filter = d3.selectAll('#filter').property('checked')
-        let newData = []
-
-        if (filter === true) {
-            for (let item of dataFromBar) {
-                if (item.isDisabled > 0) {
-                    newData.push({name: item.name, isDisabled: item.isDisabled})
-                } else {
-                    // console.log("not disability accessible")
-                }
-            }
-        } else {
-            newData = dataFromBar
-        }
-
-        data = newData
-
-        xAxis.domain(data.map(data => data.name))
-
-        svg.select(".xAxis").call(d3.axisBottom(xAxis)).exit().remove("text")
-
-        svg.selectAll(".bar")
-            .data(data)
-            .selectAll("rect")
-            .attr("fill", "#000000")
-
-        svg.select(".bar").enter().append('rect')
-
-        svg.selectAll(".bar")
-            .selectAll("rect")
-            .exit()
-            .remove("rect")
-
-
-
-
-
-
-        renderBarChart(data)
-        return data
-
-    }
-
     function renderBarChart(data) {
         //set up X axis
         //give the svg the correct size
@@ -143,4 +90,55 @@ export default function barChart(dataFromBar, selected) {
             .exit()
             .remove()
     }
+    function filterEmpty() {
+        console.log("hi");
+        const filter = d3.selectAll('#filter').property('checked')
+        let newData = []
+
+        if (filter === true) {
+            for (let item of dataFromBar) {
+                if (item.isDisabled > 0) {
+                    newData.push({name: item.name, isDisabled: item.isDisabled})
+                } else {
+                    console.log("not disability accessible")
+                }
+            }
+        } else {
+            newData = dataFromBar
+        }
+
+        data = newData
+
+        xAxis.domain(data.map(data => data.name))
+        yAxix.domain([0, max(data, data => data.isDisabled)])
+
+        svg.select(".xAxis")
+            .call(d3.axisBottom(xAxis))
+            .selectAll("text")
+            .attr("transform", "translate(-10,10)rotate(-90)")
+            .style("text-anchor", "end")
+            .exit()
+            .remove("text")
+        const bars = svg.selectAll("rect")
+            .data(data)
+        bars
+            .enter()
+            .append("rect")
+            .merge(bars)
+            .attr("x", function (data) {
+                return xAxis(data.name)
+            })
+            .attr("y", function (data) {
+                return yAxix(data.isDisabled)
+            })
+            .attr("width", xAxis.bandwidth())
+            .attr("height", function (data) {
+                return height - yAxix(data.isDisabled)
+            })
+            .attr("fill", "#8A89A6")
+        bars
+            .exit()
+            .remove()
+    }
+    renderBarChart(data)
 }
